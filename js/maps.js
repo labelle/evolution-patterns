d3b692aee4854c8335cb3b6f985d41994649e789
ross <ross@humanig.com>
2016-08-30 15:38:18 -0400

    added google maps marker

--- js/maps.js
+++ js/maps.js
@@ -1,22 +1,40 @@
   function initMap() {
     var mapDiv = document.getElementById('map1Column');
-    var map = new google.maps.Map(mapDiv, {
+    var map1 = new google.maps.Map(mapDiv, {
         center: {lat: 40.744523, lng: -73.995689},
         zoom: 13,
         scrollwheel: false
     });
 
     var mapDiv = document.getElementById('mapFullWidth');
-    var map = new google.maps.Map(mapDiv, {
+    var map2 = new google.maps.Map(mapDiv, {
         center: {lat: 40.744523, lng: -73.995689},
         zoom: 13,
         scrollwheel: false
     });
 
     var mapDiv = document.getElementById('mapFullScreen');
-    var map = new google.maps.Map(mapDiv, {
+    var map3 = new google.maps.Map(mapDiv, {
         center: {lat: 40.744523, lng: -73.995689},
         zoom: 13,
         scrollwheel: false
+    });
+
+    var marker1 = new google.maps.Marker({
+      position: {lat: 40.744523, lng: -73.995689},
+      map: map1,
+      title: 'human'
+    });
+
+    var marker2 = new google.maps.Marker({
+      position: {lat: 40.744523, lng: -73.995689},
+      map: map2,
+      title: 'human'
+    });
+
+    var marker3 = new google.maps.Marker({
+      position: {lat: 40.744523, lng: -73.995689},
+      map: map3,
+      title: 'human'
     });
   }
