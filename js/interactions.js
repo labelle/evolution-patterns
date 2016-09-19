06fb7e501bfe759fc2fab82334d9cb9e3bc6227f
Daniel Tomic <tom_danny2003@yahoo.com>
2016-09-19 12:25:20 -0400

    second attempt to fix html5 video on iPad

--- js/interactions.js
+++ js/interactions.js
@@ -380,31 +380,3 @@ if ( typeof define === 'function' && define.amd ) {
 
 
 
-// ----------------------------------------------------------------
-//             mp4 video fix for iPad/iPhones 
-// ----------------------------------------------------------------
-
-
-var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
-
-if (isIOS) {
-
-    var canvasVideo = new CanvasVideoPlayer({
-        videoSelector: '.video',
-        canvasSelector: '.canvas',
-        timelineSelector: false,
-        autoplay: true,
-        makeLoop: true,
-        pauseOnClick: false,
-        audio: false
-    });
-
-}else {
-
-    // Use HTML5 video
-    document.querySelectorAll('.canvas')[0].style.display = 'none';
-
-} 
-
-
-
