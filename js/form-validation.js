84981d2d18f62a318c5ad8f622ba2977a93672a2
Daniel Tomic <tom_danny2003@yahoo.com>
2016-09-23 15:22:06 -0400

    form-simple-password added

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,6 +1,7 @@
 document.getElementById("form-validation").onsubmit = function () {
     // var x = document.forms["form-validation"]["name"].value;
     var y = document.forms["form-validation"]["email"].value;
+    // var p = document.forms["form-validation"]["password"].value;
     // var z = document.forms["form-validation"]["telephone"].value;
 
     var submit = true;
@@ -20,6 +21,18 @@ document.getElementById("form-validation").onsubmit = function () {
         document.getElementById("email_error").innerHTML = emailSuccess;
     }
 
+
+    // if (p == null || p == "") {
+    //     console.log('nope')
+    //     passwordError = "Please enter your password";
+    //     document.getElementById("password_error").innerHTML = passwordError;
+    //     submit = false;
+    // } else {
+    //     passwordSuccess = "Thank you."
+    //     document.getElementById("password_error").innerHTML = passwordSuccess;
+    //     submit = false;
+    // }
+
     // if (z == null || z == "") {
     //     telephoneError = "Please enter your telephone";
     //     document.getElementById("telephone_error").innerHTML = telephoneError;
@@ -35,6 +48,7 @@ function removeWarning() {
 
 // document.getElementById("name").onkeyup = removeWarning;
 document.getElementById("email").onkeyup = removeWarning;
+// document.getElementById("password").onkeyup = removeWarning;
 // document.getElementById("telephone").onkeyup = removeWarning;
 
 
@@ -43,28 +57,63 @@ document.getElementById("email").onkeyup = removeWarning;
 
 
 
-// document.getElementById("form-validation-line").onsubmit = function () {
+document.getElementById("form-validation-line").onsubmit = function () {
 
-//     var y = document.forms["form-validation-line"]["email_line"].value;
+    var y = document.forms["form-validation-line"]["email1"].value;
 
-//     var submit = true;
+    var submit = true;
     
-//     if (y == null || y == "") {
-//         emailError = "Please enter your email";
-//         document.getElementById("email_error_line").innerHTML = emailError;
-//         submit = false;
-//     } else {
-//         emailSuccess = "Thank you."
-//         document.getElementById("email_error_line").innerHTML = emailSuccess;
-//     }
+    if (y == null || y == "") {
+        emailError = "Please enter your email";
+        document.getElementById("email1_error_line").innerHTML = emailError;
+        submit = false;
+    } else {
+        emailSuccess = "Thank you."
+        document.getElementById("email1_error_line").innerHTML = emailSuccess;
+    }
 
 
-//     return submit;
-// }
+    return submit;
+}
 
-// function removeWarning() {
-//     document.getElementById(this.id + "_error_line").innerHTML = "";
-// }
+function removeWarningLine() {
+    document.getElementById(this.id + "_error_line").innerHTML = "";
+}
 
 
-// document.getElementById("email_line").onkeyup = removeWarning;
+document.getElementById("email1").onkeyup = removeWarningLine;
+
+
+
+
+
+
+
+
+document.getElementById("form-validation-header").onsubmit = function () {
+
+    var y = document.forms["form-validation-header"]["email2"].value;
+
+    var submit = true;
+    
+    if (y == null || y == "") {
+        emailError = "Please enter your email";
+        document.getElementById("email2_error_header").innerHTML = emailError;
+        submit = false;
+    } else {
+        emailSuccess = "Thank you."
+        document.getElementById("email2_error_header").innerHTML = emailSuccess;
+    }
+
+
+    return submit;
+}
+
+function removeWarningHeader() {
+    document.getElementById(this.id + "_error_header").innerHTML = "";
+}
+
+
+document.getElementById("email2").onkeyup = removeWarningHeader;
+
+
